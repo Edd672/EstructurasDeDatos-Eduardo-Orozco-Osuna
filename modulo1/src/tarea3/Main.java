a85be3005ef9209579f3554f61d508b6ad552e5c
Axel-Balam-Mancera-Miramontes <manceraaxel@hotmail.com>
2023-10-26 20:22:57 -0600

    act 3

---
+++ modulo1/src/tarea3/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author Axel Balam Mancera Miramontes
+ */
+
+package tarea3;
+
+import tarea3.ui.*;
+import tarea3.utils.*;
+
+public class Main {
+    public static void main(String[] args) {
+        CLI.launchApp();
+    }
+}
